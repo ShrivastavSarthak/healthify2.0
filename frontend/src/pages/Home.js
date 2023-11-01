15dd9efeb7a9453b3371441de14931662f722c99
Sarthak Kumar <97510754+ShrivastavSarthak@users.noreply.github.com>
2023-11-01 09:53:01 +0530

    Update Home.js

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,26 +1,4 @@
-<<<<<<< HEAD
-import React, { Fragment} from 'react'
-import { Image, Card } from 'antd'
-import { motion } from 'framer-motion'
 
-
-
-import "./Home.css"
-import Auth from './Auth'
-const Home = () => {
-  
-  return (
-    <Fragment>
-      <center className='mx-3'>
-        <Auth/>
-        <motion.div className='row  mt-5'>
-          <motion.div
-            className='col d-md-none d-lg-block d-none d-sm-block d-md-block d-sm-none '
-            initial={{ opacity: 0 }}
-            transition={{ delay: 1 }}
-            whileInView={{ opacity: 1 }}>
-
-=======
 import React, { Fragment } from "react";
 import { Image, Card } from "antd";
 import { motion } from "framer-motion";
@@ -38,28 +16,26 @@ const Home = () => {
             transition={{ delay: 1 }}
             whileInView={{ opacity: 1 }}
           >
->>>>>>> 8b6b122151e2dc5c6bcadfa46cf429afbcdb8fa5
+
             <Image
               maxwidth={200}
               src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"
             />
           </motion.div>
-<<<<<<< HEAD
+
           <div className='col'>
-=======
+
           <div className="col righty">
->>>>>>> 8b6b122151e2dc5c6bcadfa46cf429afbcdb8fa5
             <motion.div
               initial={{ scale: 0 }}
               animate={{ scale: 1 }}
               transition={{ delay: 0.5 }}
               whileInView={{ opacity: 1 }}
             >
-<<<<<<< HEAD
               <Card className='p-3 ' hoverable>
                 <h3>Welcome to the </h3>
                 <h1>YourCare</h1>
-=======
+
               <Card className="p-3 card-content my-auto " hoverable>
                 <h2>Welcome to the </h2>
                 <h1
@@ -75,13 +51,11 @@ const Home = () => {
                   Discover personalized health insights, track your progress,
                   and achieve your wellness goals effortlessly."
                 </p>
->>>>>>> 8b6b122151e2dc5c6bcadfa46cf429afbcdb8fa5
               </Card>
             </motion.div>
           </div>
         </motion.div>
         <hr />
-<<<<<<< HEAD
         <h1 className='mt-5'>Services</h1>
         <div className='row mt-5 '>
           <motion.div
@@ -122,7 +96,7 @@ const Home = () => {
             initial={{ opacity: 0 }}
             transition={{ delay: 0.5 }}
             whileInView={{ opacity: 1 }}>
-=======
+
         <h1 className="mt-5">Services</h1>
         <div className="row mt-5 ">
           <motion.div
@@ -206,7 +180,6 @@ const Home = () => {
             transition={{ delay: 0.5 }}
             whileInView={{ opacity: 1 }}
           >
->>>>>>> 8b6b122151e2dc5c6bcadfa46cf429afbcdb8fa5
             <motion.div
               initial={{ scale: 0 }}
               animate={{ scale: 1 }}
@@ -218,18 +191,17 @@ const Home = () => {
             </motion.div>
           </motion.div>
           <motion.div
-<<<<<<< HEAD
+
             className='col  d-md-none d-lg-block d-none d-sm-block d-md-block d-sm-none '
             initial={{ opacity: 0 }}
             transition={{ delay: 0.5 }}
             whileInView={{ opacity: 1 }}>
-=======
+
             className="col d-sm-none d-md-block d-none d-sm-block"
             initial={{ opacity: 0 }}
             transition={{ delay: 0.5 }}
             whileInView={{ opacity: 1 }}
           >
->>>>>>> 8b6b122151e2dc5c6bcadfa46cf429afbcdb8fa5
             <Image
               maxwidth={200}
               src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"
